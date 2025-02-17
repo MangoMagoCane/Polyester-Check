24f91f2686ea270cbc0ca1d35e7151ca39860071
Leonardo Garman <garmanmago@gmail.com>
2025-02-17 04:17:55 -0500

    feat: Update Program

--- Program.cs
+++ Program.cs
@@ -1,24 +1,68 @@
 using Microsoft.Extensions.Hosting;
 using NetCord;
+using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Hosting.Services.Commands;
+using NetCord.Services;
 using NetCord.Services.ApplicationCommands;
+using NetCord.Services.Commands;
 
 namespace PolyesterCheck;
 
+public enum ClothingItems
+{
+    shirt, pants, socks, hat, underwear, watch, gloves, sweater, jacket, shorts,
+}
+
+public enum ClothingTypes
+{
+    cotton, wool, leather, denim, silk, bamboo, polyester, nylon, spandex, rayon, acrylic,
+}
+
 class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-        builder.Services.AddDiscordGateway();
+        builder.Services
+            .AddDiscordGateway()
+            .AddCommands()
+            .AddApplicationCommands();
+        IHost host = builder.Build();
+
+        host.AddModules(typeof(Program).Assembly);
+        host.UseGatewayEventHandlers();
+
+        await host.RunAsync();
     }
 }
 
-public class OhioModule : ApplicationCommandModule<ApplicationCommandContext>
+public class CustomCommandContext(Message message, GatewayClient client) : CommandContext(message, client)
 {
-    [SlashCommand("ohio", "slava ohio!")]
-    public string Ohio(User user)
+    public GuildUser BotGuildUser => Guild!.Users[Client.Id];
+}
+
+public class PolyesterModule : ApplicationCommandModule<ApplicationCommandContext>
+{
+    [SlashCommand("register", "register!")]
+    public string RegisterItem(ClothingItems item, ClothingTypes type)
     {
-        return $"{Context.User} {user}";
+        Console.WriteLine((int)item);
+        return $"{item}: {type}";
     }
+
+}
+
+public class TestModule : CommandModule<CustomCommandContext>
+{
+    [RequireContext<CustomCommandContext>(RequiredContext.Guild)]
+    [Command("test", "foo")]
+    public String Test()
+    {
+        var user = Context.BotGuildUser;
+        return user.Nickname ?? user.Username;
+    }
+
 }
